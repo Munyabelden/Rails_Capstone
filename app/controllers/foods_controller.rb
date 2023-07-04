29a64916f335ee82ea8f759d3db18b1d09356a69
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-07-04 17:54:10 +0430

    Create user controller!

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,34 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = current_user
+    @foods = current_user.foods
+  end
+
+  def new
+    @user = current_user
+    @food = Food.new
+  end
+  
+  def create
+    @food = current_user.foods.new(food_params)
+
+    if @food.save
+      redirect_to foods_path, notice: 'Your Food is created successfully!'
+    else
+      flash[:alert] = 'Something went wrong, Try again!'
+      render :new
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
