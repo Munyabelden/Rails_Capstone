eb83a67053aaa489fa245ac263987375b3effa3a
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-07-04 19:46:08 +0430

    Create food controller!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/show'
+  get 'foods/new'
   devise_for :users
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     patch 'publicize', on: :member
