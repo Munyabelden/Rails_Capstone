24649dbaeec87d06725f7acc1c56a17647dbdbb3
Gracerue <munyaradzi045@gmail.com>
2023-07-04 21:15:09 +0200

    Add styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recipes#index'
 
-
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     patch 'publicize', on: :member
   end
+  get 'recipes/public', to: 'recipes#public', as: 'public_recipes'  
     
 end
