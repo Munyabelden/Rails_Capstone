d102e8c32367d26f90d50706c5939e726d319e26
Gracerue <munyaradzi045@gmail.com>
2023-07-05 21:13:15 +0200

    Fix public controller spec

--- spec/requests/public_spec.rb
+++ spec/requests/public_spec.rb
@@ -3,18 +3,15 @@ require 'rails_helper'
 RSpec.describe PublicController, type: :controller do
   include Devise::Test::ControllerHelpers
 
+  let(:user) { User.create(name: "John Doe", email: "john@example.com", password: "password") }
+  let(:recipe) { Recipe.create(name: "Pasta Carbonara", preparation_time: "00:15", cooking_time: "00:20", description: "A delicious pasta dish.", user: user, public: true) }
+  let(:recipe1) { Recipe.create(name: "Pasta Carbonara", preparation_time: "00:15", cooking_time: "00:20", description: "A delicious pasta dish.", user: user, public: true) }
+  let(:recipe2) { Recipe.create(name: "Pasta Carbonara", preparation_time: "00:15", cooking_time: "00:20", description: "A delicious pasta dish.", user: user, public: true) }
+
   describe 'GET #index' do
-    it 'assigns public recipes to @public_recipes' do
-      public_recipe1 = create(:recipe, public: true)
-      public_recipe2 = create(:recipe, public: true)
-      private_recipe = create(:recipe, public: false)
-
-      get :index
-
-      expect(assigns(:public_recipes)).to eq([public_recipe2, public_recipe1])
-    end
 
     it 'renders the index template' do
+      sign_in user
       get :index
 
       expect(response).to render_template('index')
